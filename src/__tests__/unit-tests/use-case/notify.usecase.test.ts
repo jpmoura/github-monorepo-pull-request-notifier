86a2fff0eeb2a494c25692d7184377004fffa5e4
jpmoura <moura.joaopedro@gmail.com>
2021-06-09 08:19:31 -0300

    refactor(notify.usecase.test): refactored tests to not use IoC container
    Using from now on manual dependency injection so we can remove the setup and teardown on each test

--- src/__tests__/unit-tests/use-case/notify.usecase.test.ts
+++ src/__tests__/unit-tests/use-case/notify.usecase.test.ts
@@ -1,10 +1,9 @@
+import 'reflect-metadata';
 import * as faker from 'faker';
 import { mock, MockProxy } from 'jest-mock-extended';
 import NotifyRequest from '../../../domain/interface/request/notify-request.interface';
 import NotificationAction from '../../../domain/enum/notification-action.enum';
 import { createNotificationInputDto, createSquad } from '../../util/util';
-import container from '../../../cross-cutting/ioc/container';
-import Types from '../../../cross-cutting/ioc/types';
 import IUseCaseAsync from '../../../domain/interface/use-case/use-case-async.interface';
 import NotifyResponse from '../../../domain/interface/response/notify-response.interface';
 import IRepository from '../../../domain/interface/infra/repository/repository.interface';
@@ -12,6 +11,7 @@ import Squad from '../../../domain/model/squad.model';
 import IValidator from '../../../domain/interface/validator/validator.interface';
 import IHttpClient from '../../../domain/interface/infra/client/http-client.interface';
 import ILogger from '../../../domain/interface/infra/logger.interface';
+import NotifyUseCase from '../../../use-case/notify.usecase';
 
 function buildHeaders(): Record<string, string> {
   const headers: Record<string, string> = {};
@@ -22,30 +22,8 @@ function buildHeaders(): Record<string, string> {
   return headers;
 }
 
-function setup(
-  notifyRequestValidatorMock: MockProxy<IValidator<NotifyRequest>>,
-  squadRepositoryMock: MockProxy<IRepository<Squad>>,
-  httpClientMock: MockProxy<IHttpClient>,
-): void {
-  container.snapshot();
-
-  container.unbind(Types.SquadRepository);
-  container.unbind(Types.NotifyRequestValidator);
-  container.unbind(Types.HttpClient);
-  container.unbind(Types.Logger);
-
-  container.bind<IValidator<NotifyRequest>>(Types.NotifyRequestValidator).toConstantValue(notifyRequestValidatorMock);
-  container.bind<IRepository<Squad>>(Types.SquadRepository).toConstantValue(squadRepositoryMock);
-  container.bind<IHttpClient>(Types.HttpClient).toConstantValue(httpClientMock);
-  container.bind<ILogger>(Types.Logger).toConstantValue(mock<ILogger>());
-}
-
-function teardown(): void {
-  container.restore();
-}
-
-function getSut(): IUseCaseAsync<NotifyRequest, NotifyResponse> {
-  return container.get<IUseCaseAsync<NotifyRequest, NotifyResponse>>(Types.NotifyUseCaseAsync);
+function getSut(notifyRequestValidator: IValidator<NotifyRequest>, squadRepository: IRepository<Squad>, httpClient: IHttpClient): IUseCaseAsync<NotifyRequest, NotifyResponse> {
+  return new NotifyUseCase(squadRepository, notifyRequestValidator, httpClient, mock<ILogger>());
 }
 
 function getMock<TInterface>(): MockProxy<TInterface> {
@@ -66,16 +44,14 @@ describe('notify', () => {
     notifyRequestValidatorMock.validate.mockReturnValue({ notification: { action: faker.lorem.sentence(), pull_request: faker.lorem.sentence() } });
     const squadRepositoryMock = getMock<IRepository<Squad>>();
     const httpClientMock = getMock<IHttpClient>();
-    setup(notifyRequestValidatorMock, squadRepositoryMock, httpClientMock);
     const request = {} as NotifyRequest;
 
-    const response = await getSut().execute(request);
+    const response = await getSut(notifyRequestValidatorMock, squadRepositoryMock, httpClientMock).execute(request);
 
     expect(response.success).toBeFalsy();
     expect(notifyRequestValidatorMock.validate).toHaveBeenCalledTimes(1);
     expect(squadRepositoryMock.list).not.toHaveBeenCalled();
     expect(httpClientMock.post).not.toHaveBeenCalled();
-    teardown();
   });
 
   it.each([
@@ -94,7 +70,6 @@ describe('notify', () => {
     const notifyRequestValidatorMock = getNotifyRequestValidatorMock();
     const squadRepositoryMock = getMock<IRepository<Squad>>();
     const httpClientMock = getMock<IHttpClient>();
-    setup(notifyRequestValidatorMock, squadRepositoryMock, httpClientMock);
     const notification = createNotificationInputDto();
     notification.action = action;
     const request = {
@@ -102,13 +77,12 @@ describe('notify', () => {
       headers: buildHeaders(),
     } as NotifyRequest;
 
-    const response = await getSut().execute(request);
+    const response = await getSut(notifyRequestValidatorMock, squadRepositoryMock, httpClientMock).execute(request);
 
     expect(response.success).toBeTruthy();
     expect(notifyRequestValidatorMock.validate).toHaveBeenCalledTimes(1);
     expect(squadRepositoryMock.list).not.toHaveBeenCalled();
     expect(httpClientMock.post).not.toHaveBeenCalled();
-    teardown();
   });
 
   it('notification action is "opened" and list returns empty array then should not forward notification and return successful response', async () => {
@@ -118,19 +92,17 @@ describe('notify', () => {
     squadRepositoryMock.list.mockResolvedValue([]);
     const notifyRequestValidatorMock = getNotifyRequestValidatorMock();
     const httpClientMock = getMock<IHttpClient>();
-    setup(notifyRequestValidatorMock, squadRepositoryMock, httpClientMock);
     const request = {
       notification: createNotificationInputDto(NotificationAction.Opened, faker.git.branch()),
       headers: buildHeaders(),
     } as NotifyRequest;
 
-    const response = await getSut().execute(request);
+    const response = await getSut(notifyRequestValidatorMock, squadRepositoryMock, httpClientMock).execute(request);
 
     expect(response.success).toBeTruthy();
     expect(notifyRequestValidatorMock.validate).toHaveBeenCalledTimes(1);
     expect(squadRepositoryMock.list).toHaveBeenCalledTimes(1);
     expect(httpClientMock.post).not.toHaveBeenCalled();
-    teardown();
   });
 
   it('notification action is "opened" and list returns squads which filterToken does not match with branch prefix then should not forward notification and return successful response', async () => {
@@ -140,19 +112,17 @@ describe('notify', () => {
     squadRepositoryMock.list.mockResolvedValue([createSquad(), createSquad()]);
     const notifyRequestValidatorMock = getNotifyRequestValidatorMock();
     const httpClientMock = getMock<IHttpClient>();
-    setup(notifyRequestValidatorMock, squadRepositoryMock, httpClientMock);
     const request = {
       notification: createNotificationInputDto(NotificationAction.Opened, faker.git.branch()),
       headers: buildHeaders(),
     } as NotifyRequest;
 
-    const response = await getSut().execute(request);
+    const response = await getSut(notifyRequestValidatorMock, squadRepositoryMock, httpClientMock).execute(request);
 
     expect(response.success).toBeTruthy();
     expect(notifyRequestValidatorMock.validate).toHaveBeenCalledTimes(1);
     expect(squadRepositoryMock.list).toHaveBeenCalledTimes(1);
     expect(httpClientMock.post).not.toHaveBeenCalled();
-    teardown();
   });
 
   it('notification action is "opened" and list returns squads which filterToken does match with branch prefix and an error occur while forwarding notification then should return unsuccessful response', async () => {
@@ -164,19 +134,17 @@ describe('notify', () => {
     const notifyRequestValidatorMock = getNotifyRequestValidatorMock();
     const httpClientMock = getMock<IHttpClient>();
     httpClientMock.post.mockRejectedValueOnce(new Error('Network error'));
-    setup(notifyRequestValidatorMock, squadRepositoryMock, httpClientMock);
     const request = {
       notification: createNotificationInputDto(NotificationAction.Opened, `${filterToken}/${faker.git.branch()}`),
       headers: buildHeaders(),
     } as NotifyRequest;
 
-    const response = await getSut().execute(request);
+    const response = await getSut(notifyRequestValidatorMock, squadRepositoryMock, httpClientMock).execute(request);
 
     expect(response.success).toBeFalsy();
     expect(notifyRequestValidatorMock.validate).toHaveBeenCalledTimes(1);
     expect(squadRepositoryMock.list).toHaveBeenCalledTimes(1);
     expect(httpClientMock.post).toHaveBeenCalledTimes(1);
-    teardown();
   });
 
   it('notification action is "opened" and list returns squads which filterToken does match with branch prefix then should forward notification to squad webhook and return successful response', async () => {
@@ -187,19 +155,17 @@ describe('notify', () => {
     const squadRepositoryMock = getMock<IRepository<Squad>>();
     squadRepositoryMock.list.mockResolvedValue([createSquad(), createSquad(filterToken)]);
     const httpClientMock = getMock<IHttpClient>();
-    setup(notifyRequestValidatorMock, squadRepositoryMock, httpClientMock);
 
     const request = {
       notification: createNotificationInputDto(NotificationAction.Opened, `${filterToken}/${faker.git.branch()}`),
       headers: buildHeaders(),
     } as NotifyRequest;
 
-    const response = await getSut().execute(request);
+    const response = await getSut(notifyRequestValidatorMock, squadRepositoryMock, httpClientMock).execute(request);
 
     expect(response.success).toBeTruthy();
     expect(notifyRequestValidatorMock.validate).toHaveBeenCalledTimes(1);
     expect(squadRepositoryMock.list).toHaveBeenCalledTimes(1);
     expect(httpClientMock.post).toHaveBeenCalledTimes(1);
-    teardown();
   });
 });
